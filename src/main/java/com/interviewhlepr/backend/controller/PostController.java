dcc4032b9c0adc70898ff763d2c912455e3fe4c3
홍석현 <seokh1213@gmail.com>
2023-03-19 01:38:54 +0900

    feat: post 조회

--- src/main/java/com/interviewhlepr/backend/controller/PostController.java
+++ src/main/java/com/interviewhlepr/backend/controller/PostController.java
@@ -3,6 +3,7 @@ package com.interviewhlepr.backend.controller;
 import com.interviewhlepr.backend.annotation.AuthResult;
 import com.interviewhlepr.backend.exception.CommonException;
 import com.interviewhlepr.backend.model.dto.BaseResponse;
+import com.interviewhlepr.backend.model.dto.PageDTO;
 import com.interviewhlepr.backend.model.dto.PostDTO;
 import com.interviewhlepr.backend.model.dto.PostRequestDTO;
 import com.interviewhlepr.backend.model.entity.User;
@@ -12,6 +13,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,15 +33,23 @@ public class PostController {
     private final PostQueryService postQueryService;
 
     @GetMapping
-    public BaseResponse getPostList(@RequestParam(defaultValue = "0") int page) {
-        if (page < 0) {
+    public BaseResponse getPostList(@RequestParam(defaultValue = "1") int page) {
+        if (page < 1) {
             throw CommonException.INVALID_PARAMETER;
         }
 
-        Page<PostDTO> postPage = postQueryService.getPostPage(page, POST_LIST_SIZE);
+        Page<PostDTO> postPage = postQueryService.getPostList(page - 1, POST_LIST_SIZE);
 
-        return BaseResponse.of(postPage);
+        return BaseResponse.of(PageDTO.of(postPage));
     }
+
+    @GetMapping("/{postId}")
+    public BaseResponse getPostList(@PathVariable Long postId) {
+        PostDTO postDTO = postQueryService.getPost(postId);
+
+        return BaseResponse.of(postDTO);
+    }
+
 
     @PostMapping(value = "/temp/write", consumes = {"multipart/form-data"})
     public BaseResponse temporaryWritePost(@AuthResult User user,
--- src/main/java/com/interviewhlepr/backend/service/PostQueryService.java
+++ src/main/java/com/interviewhlepr/backend/service/PostQueryService.java
@@ -4,23 +4,30 @@ import com.interviewhlepr.backend.mapper.PostMapper;
 import com.interviewhlepr.backend.model.dto.PostDTO;
 import com.interviewhlepr.backend.model.entity.Post;
 import com.interviewhlepr.backend.repository.PostRepository;
-import com.interviewhlepr.backend.repository.PostTagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PostQueryService {
-    private final PostTagRepository postTagRepository;
     private final PostRepository postRepository;
     private final PostMapper postMapper;
 
-    public Page<PostDTO> getPostPage(int page, int size) {
+    @Transactional
+    public Page<PostDTO> getPostList(int page, int size) {
         PageRequest pageRequest = PageRequest.of(page, size);
         Page<Post> postPage = postRepository.findAll(pageRequest);
         return new PageImpl<>(postPage.getContent().stream().map(postMapper::toDTO).toList(), pageRequest, postPage.getTotalElements());
+    }
+
+    @Transactional
+    public PostDTO getPost(Long postId) {
+        return postRepository.findById(postId)
+                .map(postMapper::toDTO)
+                .orElse(null);
     }
 }
