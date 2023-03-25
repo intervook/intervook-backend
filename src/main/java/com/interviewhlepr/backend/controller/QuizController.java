160964cc6e0f397a35b002f6e331eec38260e3fc
홍석현 <seokh1213@gmail.com>
2023-03-26 02:27:21 +0900

    feat: quiz controller 생성

--- src/main/java/com/interviewhlepr/backend/controller/QuizController.java
+++ src/main/java/com/interviewhlepr/backend/controller/QuizController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,7 +48,7 @@ public class QuizController {
         return BaseResponse.of(quiz);
     }
 
-    @PostMapping("/update")
+    @PutMapping("/update")
     public BaseResponse updateQuiz(@Valid @RequestBody QuizDTO quizDTO) {
         if (quizDTO.id() == null) {
             throw CommonException.INVALID_PARAMETER;
